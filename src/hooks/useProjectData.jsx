602611e54bfea0724f4d0821ac56da805fa15550
Zoe Mwangi <your-email@example.com>
2025-05-05 22:50:13 +0100

    changes fixed

--- src/hooks/useProjectData.jsx
+++ src/hooks/useProjectData.jsx
@@ -4,12 +4,12 @@ const useProjectData = create((set) => ({
   projectData: null,
   filteredProjects: [],
   projectType: "all",
-  authenticated: true,
+  authenticated: false,
   allProjects: [],
   setAllProjects: (projects)=> set({allProjects: projects}),
   setFilteredProjects: (filtered) => set({filteredProjects: filtered}),
   setProjectData: (data) => set({ projectData: data }),
   setProjectType: (projectType)=> set({projectType}),
-  setAuthenticated: (authenticated)=> set({authenticated: false})
+  setAuthenticated: (authenticated)=> set({authenticated: true})
 }));
 export default useProjectData;
